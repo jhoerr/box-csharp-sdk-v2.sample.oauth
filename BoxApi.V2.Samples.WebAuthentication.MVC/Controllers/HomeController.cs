5a8d0ef2c8d59c349d47493058e0e4b304c31f6e
John Hoerr <jhoerr@gmail.com>
2012-12-22 16:00:07 -0500

    Adding existing solution

---
+++ BoxApi.V2.Samples.WebAuthentication.MVC/Controllers/HomeController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+using BoxApi.V2.Authentication.OAuth2;
+using BoxApi.V2.Samples.WebAuthentication.MVC.Models;
+
+namespace BoxApi.V2.Samples.WebAuthentication.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region constants
+        private const string JsonFormat =
+            @"{{
+    ""ClientId"": ""{0}"",
+    ""ClientSecret"": ""{1}"",
+    ""AccessToken"": ""{2}"",
+    ""RefreshToken"": ""{3}"",
+    ""CollaboratingUserEmail"": ""box.csharp.sdk@gmail.com"",
+    ""CollaboratingUserId"": ""186800768""
+}}";
+
+        private const string ClientId = "clientId";
+        private const string ClientSecret = "clientSecret";
+        private const string AccessToken = "accessToken";
+        private const string RefreshToken = "refreshToken";
+        #endregion
+
+        public ActionResult Index()
+        {
+            return IsError()
+                       ? Error(Request.QueryString["error"], Request.QueryString["error_description"], Request.QueryString["state"])
+                       : IsCode()
+                             ? Token(Request.QueryString["code"])
+                             : Start();
+        }
+
+        private ActionResult Start()
+        {
+            ClearSession();
+            return View();
+        }
+
+        private void ClearSession()
+        {
+            Session[ClientId] = null;
+            Session[ClientSecret] = null;
+            Session[AccessToken] = null;
+            Session[RefreshToken] = null;
+        }
+
+        public ActionResult InitiateAuthorization(string clientId, string clientSecret)
+        {
+            Session[ClientId] = clientId;
+            Session[ClientSecret] = clientSecret;
+            var boxAuthenticator = new TokenProvider(clientId, clientSecret);
+            return new RedirectResult(boxAuthenticator.GetAuthorizationUrl());
+        }
+
+        private bool IsCode()
+        {
+            return Request.QueryString["code"] != null;
+        }
+
+        private bool IsError()
+        {
+            return Request.QueryString["error"] != null;
+        }
+
+        private ActionResult Token(string arg1)
+        {
+            try
+            {
+                var boxAuthenticator = new TokenProvider(Session[ClientId] as string, Session[ClientSecret] as string);
+                OAuthToken accessToken = boxAuthenticator.GetAccessToken(arg1);
+                Session[AccessToken] = accessToken.AccessToken;
+                Session[RefreshToken] = accessToken.RefreshToken;
+                return View("Authorize", accessToken);
+            }
+            catch (BoxException e)
+            {
+                return Error(e.Error.Status, e.Message);
+            }
+            catch (Exception e)
+            {
+                return Error(e.Message, e.StackTrace);
+            }
+        }
+
+        private ViewResult Error(string error, string description, string state = null)
+        {
+            ClearSession();
+            return View("Error", new ErrorModel {Message = error, Description = description ?? "(none)", State = state ?? "(none)"});
+        }
+
+        public FileResult Save()
+        {
+            string result = string.Format(JsonFormat, Session[ClientId] as string, Session[ClientSecret] as string, Session[AccessToken] as string, Session[RefreshToken] as string);
+            ClearSession();
+            return File(Encoding.UTF8.GetBytes(result), "application/json", "test_info.json");
+        }
+    }
+}
---
+++ BoxApi.V2.Samples.WebAuthentication.MVC/readme.txt
@@ -0,0 +1,37 @@
+﻿Thanks for trying out the Box SDK for the v2 API!
+
+This sample site will provide you with a working example of how to get 
+through the Box OAuth2 authentication flow.
+
+*************************************************
+* KEEP READING; YOU MUST DO A FEW THINGS FIRST! *
+*************************************************
+
+Part of the OAuth2 workflow involves an HTTP redirect from Box's site to the 
+one you'll be running here. The URL of *this* demo site *must* be registered with 
+Box in order for everything to work. (This pre-registration is done in order 
+to prevent rogue redirects; it's part of the OAuth2 spec.)
+
+1. In the Solution Explorer, click on this project.
+2. Press F4 to bring up the project's Properties.
+3. Change 'SSL Enabled' from False to True.  The SSL URL field should be 
+   automatically populated.
+4. Copy the SSL URL to the clipboard.
+5. Browse to http://developers.box.com
+6. Click on "My Box Apps" on the upper right; log in.
+7. Edit the application that you want to work with.
+8. Under 'OAuth2 Parameters' locate the 'redirect_uri' field.
+9. Paste the SSL URL into this field.  Save your changes.
+
+This tells Box to redirect you back to this site after you've authenticated 
+and agreed to let your application access your Box data.
+
+Ok, that's it -- everything should be ready to go now.  You might want to 
+keep the browser window open that contains your Box application's Client
+ID and Client Secret; you'll need those in a moment.
+
+Press F5 to get started!
+
+
+
+
